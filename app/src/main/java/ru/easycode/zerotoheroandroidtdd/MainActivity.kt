c73aaf6f59e694ba5d9f009adfd298b613e7fece
GorokhovT <timofey.gorokhov@cse.ru>
2024-01-17 14:58:43 +0300

    14 - solved_2

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,19 +1,21 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.util.Timer
+import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var progressBar:ProgressBar
+    private lateinit var progressBar: ProgressBar
     private lateinit var rootLayout: LinearLayout
     private lateinit var titleTextView: TextView
-    private lateinit var actionButton:Button
+    private lateinit var actionButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,11 +28,28 @@ class MainActivity : AppCompatActivity() {
         actionButton.setOnClickListener {
             actionButton.isEnabled = false
             progressBar.visibility = View.VISIBLE
+
+            Timer().schedule(object : TimerTask() {
+                override fun run() = runOnUiThread {
+                    titleTextView.visibility = View.VISIBLE
+                    progressBar.visibility = View.GONE
+                    actionButton.isEnabled = true
+                }
+            }, 3000)
+
+            /*
+            Handler(Looper.getMainLooper()).postDelayed({
+                titleTextView.visibility = View.VISIBLE
+                progressBar.visibility = View.GONE
+                actionButton.isEnabled = true
+            }, 3000)
+
             actionButton.postDelayed({
                 titleTextView.visibility = View.VISIBLE
                 progressBar.visibility = View.GONE
                 actionButton.isEnabled = true
             }, 3000)
+             */
         }
     }
 }
